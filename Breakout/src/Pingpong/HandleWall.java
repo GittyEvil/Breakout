1e272b0e42f5d1832b609cf678ad9837a7f029fe
Adrian S <89584348+GittyEvil@users.noreply.github.com>
2024-02-14 12:09:49 +0100

    fixade poängsystemet
    lvl 1 bricka ger 1 poäng 
    lvl 2 bricka ger 2 poäng 
    lvl 3 bricka ger 3 poäng

---
+++ Breakout/src/Pingpong/HandleWall.java
@@ -0,0 +1,57 @@
+package Pingpong;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.*;
+
+public class HandleWall {
+	private ArrayList<Wall> Walls;
+
+	public HandleWall() {
+		Walls = new ArrayList<>();
+		
+			Wall vänsterVägg = new Wall(0,0,1,Const.gameboardHeight,false,false,true,Color.red);
+			Wall högerVägg = new Wall(Const.gameboardWidth - 1 ,0,1,Const.gameboardHeight,false,true,false,Color.red);
+			Wall toppVägg = new Wall(0,0,Const.gameboardWidth,1,true,false,false,Color.red);
+			Walls.add(vänsterVägg); //vänster vägg
+			Walls.add(högerVägg); //höger vägg
+			Walls.add(toppVägg); //topp vägg
+ 		
+	}
+	public void update(Keyboard keyboard) {
+	    for(Wall wall: Walls) {
+	        wall.update(keyboard);
+	        for (Ball ball : Game.balls.getBalls()) {
+	            if (wall.getBounds().intersects(ball.getBounds())) {
+	            	//toppvägg
+	                if(wall.isTopWall()) {
+	                    if (ball.getDy() < 0 && ball.getY() <= wall.getY() + wall.getHeight()) {
+	                        ball.setDy(-ball.getDy());
+	                    }
+	                } 
+		            //högervägg
+                	if (wall.isRightWall()) {
+                	    if (ball.getDx() > 0 && ball.getX() >= wall.getX() - ball.getWidth() ) {
+                	        ball.setDx(-ball.getDx());
+                	    }
+                	}
+	                //vänstervägg
+                	if(wall.isLeftWall()) {
+                		if (ball.getDx() < 0 && ball.getX() <= wall.getX() + wall.getWidth()) {
+	                        ball.setDx(-ball.getDx());
+	                    }
+                	}
+	                
+	            }
+	        }
+	    }
+	}
+
+
+
+	public void draw(Graphics2D graphics) {
+		for(Wall wall: Walls) {
+			wall.draw(graphics);
+		}
+ 	}
+}
