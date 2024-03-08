d309045ebfe9ef8c6af8dc3f47311e4b22d120eb
Evilll <adrian.stude@gmail.com>
2024-03-08 12:13:17 +0100

    gjorde om sort med comparator för att sortera rätt ändå

--- src/Pingpong/HighScores.java
+++ src/Pingpong/HighScores.java
@@ -2,6 +2,7 @@ package Pingpong;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.swing.DefaultListModel;
@@ -36,19 +37,28 @@ public class HighScores<E> {
 	}
 	
 	public void sort(DefaultListModel Lista) {
-		//som iterator lista, "kopierar av org listan för att hantera sortering"
-		ArrayList<String>  tempLista = new ArrayList<>();
-		for (int i = 0; i < Lista.size(); i++) {
-			tempLista.add((String) Lista.get(i));
-		}
-		//lägger in största talen först
-		Collections.sort(tempLista, Collections.reverseOrder());
-		//tömmer och lägger in de sorterade talen i listan i rätt ordning
-		Lista.removeAllElements();
-		for (String s : tempLista) {
-			Lista.addElement(s);
-		}
-	}
+		//som iterator lista, "kopierar" av org listan för att hantera sortering
+        ArrayList<String> tempLista = new ArrayList<>();
+        for (int i = 0; i < Lista.size(); i++) {
+            tempLista.add((String) Lista.getElementAt(i));
+        }
+        //sorterar in lista med en comporator som tar och sorterar så störst tal är först(skiter nu i namn pga comparator)
+        Collections.sort(tempLista, new Comparator<String>() {
+            @Override
+            //delar upp 2 strings i taget och jämför endast poäng, letar efter mellanslaget mellan namn å poäng
+            public int compare(String o1, String o2) {
+            	//delar upp, och gör om string poäng till int poäng för att kunna jämföra
+                int score1 = Integer.parseInt(o1.substring(o1.lastIndexOf(" ") + 1));
+                int score2 = Integer.parseInt(o2.substring(o2.lastIndexOf(" ") + 1));
+                return Integer.compare(score2, score1);
+            }
+        });
+        //tömmer och lägger in de sorterade talen i listan i rätt ordning
+        Lista.clear();
+        for (String entry : tempLista) {
+            Lista.addElement(entry);
+        }
+    }
 
 	
 	public void isFull() {
